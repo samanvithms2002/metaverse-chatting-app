47744260c8e5ceb09c7133ff291b6290526da2a7
Samanvith M S <samanvith2002@gmail.com>
2022-02-05 18:37:51 +0530

    fixed err in logout onclick method

--- components/Login.tsx
+++ components/Login.tsx
@@ -6,7 +6,7 @@ export function Login() {
     <div className="h-screen flex flex-col items-center space-y-5 justify-center bg-slate-900 align-middle text-white">
       <h1 className="text-3xl font-bold">Welcome to Metaverse!</h1>
       <div>
-        <button type="button" onClick={authenticate} className="bg-blue-800 p-5 rounded-md font-bold">Login to the Chatting app</button>
+        <button type="button" onClick={()=>authenticate()} className="bg-blue-800 p-5 rounded-md font-bold">Login to the Chatting app</button>
       </div>
     </div> 
   )
