16f7c76b4249431ebd8f7feaff363cf841e0d545
Samanvith M S <samanvith2002@gmail.com>
2022-02-05 18:27:50 +0530

    first commit

---
+++ components/Login.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { useMoralis } from "react-moralis";
+export function Login() {
+  const {authenticate} = useMoralis();
+  return (
+    <div className="h-screen flex flex-col items-center space-y-5 justify-center bg-slate-900 align-middle text-white">
+      <h1 className="text-3xl font-bold">Welcome to Metaverse!</h1>
+      <div>
+        <button type="button" onClick={authenticate} className="bg-blue-800 p-5 rounded-md font-bold">Login to the Chatting app</button>
+      </div>
+    </div> 
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <MoralisProvider initializeOnMount appId={process.env.NEXT_PUBLIC_APP_ID!} serverUrl={process.env.NEXT_PUBLIC_SERVERURL!}>
+    <Component {...pageProps} />
+  </MoralisProvider>
+  )
 }
 
 export default MyApp
